4ba4e29f0b7ad8ef457807cc6d6352efa7d1a8be
khrisyu9 <khrisyu@umich.edu>
2022-01-07 15:50:34 -0500

    add the schedule data preprocessing

---
+++ schedule_data_cleaning.R
@@ -0,0 +1,31 @@
+library(dplyr)
+
+# read and filter players with enough game attendance and minutes
+#read all .csv documents
+setwd("E:/Bayes_copula/Survival_analysis/2018-2019gamedata/schedule")
+oct <- read.csv(file = "oct_schedule.csv", na.strings=c(""," ","NA"))
+nov <- read.csv(file = "nov_schedule.csv", na.strings=c(""," ","NA"))
+dec <- read.csv(file = "dec_schedule.csv", na.strings=c(""," ","NA"))
+jan <- read.csv(file = "jan_schedule.csv", na.strings=c(""," ","NA"))
+feb <- read.csv(file = "feb_schedule.csv", na.strings=c(""," ","NA"))
+mar <- read.csv(file = "mar_schedule.csv", na.strings=c(""," ","NA"))
+apr <- read.csv(file = "apr_schedule.csv", na.strings=c(""," ","NA"))
+
+schedule <- rbind(oct, nov)
+schedule <- rbind(schedule, dec)
+schedule <- rbind(schedule, jan)
+schedule <- rbind(schedule, feb)
+schedule <- rbind(schedule, mar)
+schedule <- rbind(schedule, apr)
+
+names(schedule)[1] <- 'Date'
+names(schedule)[4] <- 'PTS.Visitor'
+names(schedule)[6] <- 'PTS.Home'
+
+schedule$VisitorABBR <- "NA"
+schedule$HomeABBR <- "NA"
+
+# write.csv(schedule, file = "schedule.csv")
+schedule_new <- read.csv(file = "schedule.csv")
+schedule_new$Date1 <- as.Date(schedule_new$Date, "%a, %b %d, %Y")
+
